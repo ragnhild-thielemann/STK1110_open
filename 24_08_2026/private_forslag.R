84ec2020b9c1c4a124533279da6a2cf2cbce07bd
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-24 09:26:46 +0200

    repersentantforsalg

---
+++ 24_08_2026/private_forslag.R
@@ -0,0 +1,8 @@
+library(readr)
+library (tidyverse)
+data = read.csv("C:/Users/ragnh/OneDrive/Dokumenter/GitHub/STK1110_open/24_08_2026/privat_S.csv", sep = ";", header = TRUE)
+
+
+privat = ggplot(data, aes(x = Periode)) + geom_bar() + labs(x = "Periode", y = "Antall forslag", title = "Private forslag i stortinget")
+
+ggsave("private_forslag.png", plot = privat)
