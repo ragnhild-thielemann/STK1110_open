1f5575955d8622b08aae8a62bddf47e672682766
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-24 12:26:55 +0200

    flyavganger

---
+++ 24_08_2026/flyavganger.R
@@ -0,0 +1,31 @@
+
+library(tidyverse)
+library(nycflights13) #har importert pakken, og bruker nå dette biblioteket, der det er et innebygd datasett
+
+
+#Vi ønsker å lage nye dataframes, der vi betinger variablene i framen vår
+
+b = flights |> 
+  filter(dep_delay > 120) #dette er alle linjene i framen, der forsinkelesen er over 120 minutter
+
+a = flights |> filter(dep_delay > 120) |> filter(dep_time > 900) #kan legge ved flere betingelser
+
+#skal finne alle flyene som dro første januar
+
+januar1 = flights |> filter(month %in% c(1,2))
+
+print(januar1)
+
+sortert_dato = flights |> arrange(year,month, day)
+
+
+
+delay = flights |> filter(dep_delay>(60*2))
+
+f = ggplot(delay , mapping = aes(x = dep_delay, y = arr_delay))  + geom_point() + labs(x = "Depature", y = "Arrival", title = "Korrelerte forsinkelser")
+ggsave("cor_forsinkelser.png",plot = f)
+
+#Forsiknet, men klarte å ta igjen -> tar det dobbelt
+a = flights |> filter(dep_delay> 120 & arr_delay < (dep_delay-30)) 
+View(a)
+
--- 24_08_2026/forste_script.R
+++ 24_08_2026/forste_script.R
@@ -2,15 +2,19 @@
 
 library(readr)
 library (tidyverse)
-data = read.csv("C:/Users/ragnh/OneDrive/Dokumenter/GitHub/STK1110_open/S.csv", sep = ";", header = TRUE)
+data_storting = read.csv("C:/Users/ragnh/OneDrive/Dokumenter/GitHub/STK1110_open/S.csv", sep = ";", header = TRUE)
 
 
 #Det første jeg vil gjøre er å finne kjønnsfordelingen på Stortinget
 
 #da vi har  norske bokstaver, må vi endre på variabelnavnene
-names(data)[5] = "ar"
-names(data)[4] <- "Kjonn"
+names(data_storting)[5] = "ar"
+names(data_storting)[4] <- "Kjonn"
 
+ar_vektor = c(data_storting$ar)
+par(mfrow = (c(1,2)))
+kjonn_fordeling = ggplot(data_storting,aes(x = Kjonn)) + geom_bar() + labs(x = "Kjonn", y = "Antall", title = "Kjonnsfordeling")
+ar_fordeling = ggplot(data_storting,aes(x = ar)) + geom_bar() + labs(x = "Fodselsar", y = "Antall", title = "Aldersfordeling")
 
-ar_fordeling = ggplot(data,aes(x = ar)) + geom_bar() + labs(x = "Fodselsar", y = "Antall", title = "Aldersfordeling")
-ggsave("Aldersfordeling.png",plot = ar_)
+par(mfrow = c(1, 1))
+ggsave("Aldersfordeling.png",plot = ar_fordeling)
