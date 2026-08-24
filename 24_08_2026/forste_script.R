7405b8d85f88eaeb60ab7f8851bfb1b31e6fd466
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-24 08:37:41 +0200

    ja

--- 24_08_2026/forste_script.R
+++ 24_08_2026/forste_script.R
@@ -1,5 +1,16 @@
 
 
-print("Hello world")
+library(readr)
+library (tidyverse)
+data = read.csv("C:/Users/ragnh/OneDrive/Dokumenter/GitHub/STK1110_open/S.csv", sep = ";", header = TRUE)
 
-print("new branch")
+
+#Det første jeg vil gjøre er å finne kjønnsfordelingen på Stortinget
+
+#da vi har  norske bokstaver, må vi endre på variabelnavnene
+names(data)[5] = "ar"
+names(data)[4] <- "Kjonn"
+
+
+ar_fordeling = ggplot(data,aes(x = ar)) + geom_bar() + labs(x = "Fodselsar", y = "Antall", title = "Aldersfordeling")
+ggsave("Aldersfordeling.png",plot = ar_)
