2fdfcdae7fbb13218ff3b6646e038ddbdabda663
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-25 19:35:45 +0200

    ja

--- ECON/Seminar_2/loktra-volterra-2.R
+++ ECON/Seminar_2/loktra-volterra-2.R
@@ -32,5 +32,5 @@ for (i in 2:T){
 }
 
 lv = data.frame(Tid = T_v,Fox  = F_v , Rabbit = R_v)
-s = ggplot(lv) + geom_point(aes(x = Tid, y = Fox, color = "Fox")) + geom_point(aes(x = Tid, y = Rabbit,color = "Rabbit")) + labs(x = "Time", y = "Antall i bestanden", title = "Lotka-volterra")
-ggsave("dode_harer.png", plot = s)
+abs= ggplot(lv) + geom_point(aes(x = Tid, y = Fox, color = "Fox")) + geom_point(aes(x = Tid, y = Rabbit,color = "Rabbit")) + labs(x = "Time", y = "Antall i bestanden", title = "Lotka-volterra")
+ggsave("dode_harer.png", plot = abs)
