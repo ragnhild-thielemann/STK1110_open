11a701e729e028f3fa01f9af180ab13819984837
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-25 12:13:09 +0200

    ja

--- ECON/Trying_and_failing/Seminar_4/task_1.R
+++ ECON/Trying_and_failing/Seminar_4/task_1.R
@@ -13,3 +13,11 @@ co2_USA_kina = ggplot() + geom_smooth(data = data_USA, aes(x = Year, y = co2,col
 
 ggsave("co2_USA_kina.png",plot = co2_USA_kina)
 
+#lager en ny variabel for økning i utslipp
+#legger til den nye kolonnen i det eksisterende datasettet
+
+co2 = co2 |> mutate("growth" = (co2-lag(co2))/lag(co2))
+
+co2_USA_kina_ok = ggplot() + geom_smooth(data = co2[co2$Code== "USA",], aes(x = Year, y = growth, color = "USA")) + geom_smooth(data = co2[co2$Code == "CHN",] , aes(x = Year, y = growth, color = "China") ) + labs(x = "year", y = "utslipp", title = "CO2-utslipp")
+                                                                                                                                                
+
