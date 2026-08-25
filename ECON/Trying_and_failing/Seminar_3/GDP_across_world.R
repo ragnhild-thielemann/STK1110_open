9a4cf93cade4353ed033f0237282a88217c21ea8
Ragnhild Thielemann <ragnth@ulrik.uio.no>
2026-08-25 11:39:10 +0200

    ja

--- ECON/Trying_and_failing/Seminar_3/GDP_across_world.R
+++ ECON/Trying_and_failing/Seminar_3/GDP_across_world.R
@@ -21,12 +21,32 @@ print(pwt)
 china = pwt |> filter(country == "China" & year >= 1970 & year <=2000) |> mutate("vekst" = (gdp-lag(gdp))/lag(gdp)) #mutate lager en ny kolonne i data_framen
 
 #finner logaritmen til gdp i årenen henholdsvis 1970 og 2000
-gdp_1970 = log(pwt$gdp[pwt$year == 1970])
-gdp_2000 = log(pwt$gdp[pwt$year == 2000])
+gdp_1970 = (pwt$gdp[pwt$year == 1970])
+gdp_2000 = (pwt$gdp[pwt$year == 2000])
 
 
 a = pwt$country[pwt$year== "1970"]
 print(a)
-pwt.growth = data.frame( "Land" = a, "1970" = gdp_1970 ,"2000" =  gdp_2000)
-view(pwt.growth)
+growth = data.frame( "Land" = a, "1970" = gdp_1970 ,"2000" =  gdp_2000)
+
+
+#lager en vektor med logaritmen til gdp. Dette blir lagt til som en ekstra kolonne
+pwt$lgdp = log(pwt$gdp)
+growth = growth |> mutate("growth" = (growth[,2] - growth[,3])/growth[,3])
+print(growth$X1970)
+
+#tar logaritmen av de to, og setter disse som kolonnene i data-framen
+growth$X1970 = log(growth$X1970)
+growth$X2000 = log(growth$X2000)
+
+p = ggplot(growth) + geom_point(aes(x = X1970, y = growth)) + labs(x = "Logaritmen av BNP - 1970" , y = "vekst 1970-2000" , title = "Vekst som funksjon av BNP")
+
+ggsave("Vekst_BNP.png", plot = p)
+
+#lager en dummyvariabel som en egen kolonne, om hvorhvidt gdp vokser eller ikke
+pwt_growth = pwt |> mutate("vekst" = ifelse((gdp-lag(gdp))/lag(gdp)<0,0,1))
+
+
+#finner forveningsrverdien til antallet år det har vært vekst i bnp , na.rm = TRUE -> garanterer at det er et datapunkt der.
+pwt_fraction = pwt_growth |> group_by(country) |> summarise(mean(vekst, na.rm = TRUE))
 
